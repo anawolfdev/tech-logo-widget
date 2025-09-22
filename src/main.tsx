32fd0c9b922d09cf572f9b7451dc684831b7767e
Anawolfdev <amarovazquezana@gmail.com>
2025-09-22 13:57:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import LogoPage from "./LogoPage";
+import './global.css';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
